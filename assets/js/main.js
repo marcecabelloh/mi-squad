0d6bed701d0327e995869e914269228006748722
marcecabelloherrera <marcecabello.h@gmail.com>
2017-05-05 00:33:20 -0300

    correción html y creacion de función que imprime los datos en html

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,22 @@ function miembroSquad(nombre, apellido, edad, hobbies){
 	this.hobbies = hobbies;
 }
 
-var melissa = new miembroSquad("Melissa", "Pacheco", "25", "dormir, comer, Matilda <3");
-var paulina = new miembroSquad("Paulina", "Aros", "28", "hacer libretas, acariciar gatos, manicure");
-var karla = new miembroSquad("Karla", "Jeria", "35", "comer chocolates, viajar, ir al cine");
-var tanya = new miembroSquad("Tanya", "Ramirez", "2?", "gatos, viajar, tomar tecito a las 4");
-var amala = new miembroSquad("Valentina Amala", "Kamala", "29", "bordar, tejer, dormir");
-var paula = new miembroSquad("Paula", "Ponce", "28", "música, aves, Héroes of the storm");
-var mariela = new miembroSquad("Mariela", "Cubillos", "36", "tejer, comer chocolates, cocinar");
-var marcela = new miembroSquad("Marcela", "Cabello", "31", "cantar, yoga, cocinar");
+var melissa = new miembroSquad("Melissa Pacheco", "25", "dormir, comer, Matilda <3");
+
+var paulina = new miembroSquad("Paulina Aros", "28", "hacer libretas, acariciar gatos, manicure");
+
+var karla = new miembroSquad("Karla Jeria", "35", "comer chocolates, viajar, ir al cine");
+
+var tanya = new miembroSquad("Tanya Ramirez", "2?", "gatos, viajar, tomar tecito a las 4");
+
+var amala = new miembroSquad("Valentina Amala Kamala", "29", "bordar, tejer, dormir");
+
+var paula = new miembroSquad("Paula Ponce", "28", "música, aves, Héroes of the storm");
+
+var mariela = new miembroSquad("Mariela Cubillos", "36", "tejer, comer chocolates, cocinar");
+
+var marcela = new miembroSquad("Marcela Cabello", "31", "cantar, yoga, cocinar");
+
 
 var array = [];
 
@@ -25,4 +33,15 @@ array.push(paula);
 array.push(mariela);
 array.push(marcela);
 
+function listaSquad(){
 
+	var squad = document.getElementById("lista"); 
+	array.forEach(function(elementos){ 
+		squad.innerHTML += "<b>Nombre:</b>" + elementos.nombre + elementos.apellido + "<b>Edad:</b>" + elementos.edad + "<b>Hobbie:</b>" + elementos.hobbies;
+
+	});
+
+	listaSquad();
+
+
+	
