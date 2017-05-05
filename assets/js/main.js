8df45a06e8470adbb9c334f04e5df385696bcabf
marcecabelloherrera <marcecabello.h@gmail.com>
2017-05-05 10:08:55 -0300

    agregué comentarios en el js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+//ffunción constructora 
 function MiembroSquad(nombre, apellido, edad, hobbies){
 	this.nombre = nombre;
 	this.apellido = apellido;
@@ -8,8 +9,9 @@ function MiembroSquad(nombre, apellido, edad, hobbies){
 
 function listaSquad(){
 
-var array = [];
+var array = [];//arreglo vacío para guardar cada uno de los miembros de mi squad
 
+//instancia para cada integrante del squad
 var melissa = new MiembroSquad("Melissa", "Pacheco", 25, ["dormir", "comer", "Matilda <3"]);
 var paulina = new MiembroSquad("Paulina", "Aros", 28, ["hacer libretas", "acariciar gatos", "manicure"]);
 var karla = new MiembroSquad("Karla", "Jeria", 35, ["comer chocolates", "viajar", "ir al cine"]);
@@ -19,6 +21,7 @@ var paula = new MiembroSquad("Paula", "Ponce", 28, ["música", "aves", "Héroes 
 var mariela = new MiembroSquad("Mariela", "Cubillos", 36, ["tejer", "comer chocolates", "cocinar"]);
 var marcela = new MiembroSquad("Marcela","Cabello", 31, ["cantar", "yoga", "cocinar"]);
 
+//push para introducir los datos a mi array vacío
 array.push(melissa);
 array.push(paulina);
 array.push(karla);
@@ -28,7 +31,7 @@ array.push(paula);
 array.push(mariela);
 array.push(marcela);
 
-
+//muestra los datos de los miembros del squad en un div en el html 
 var squad = array.forEach(function(elementos){
 		document.getElementById("lista").innerHTML += "<b>Nombre: </b>" + elementos.nombre + " " + elementos.apellido + "<br><b>Edad: </b>" + elementos.edad + "<br><b>Hobbies:</b>" + "<ul><li>" + elementos.hobbies[0] + "</li><li>" + elementos.hobbies[1] + "</li><li>" + elementos.hobbies[2] + "</li></ul>";
 	})
